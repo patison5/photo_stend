e80acb1a7c8f7f0dd3f5fe8baef7749e071a73be
patison5 <patison4@yandex.ru>
2020-10-09 10:15:22 +0300

    checking rotation animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log("hello")
+var data = [
+	{
+		"title": "Елизавета Залупкина",
+		"image": "../images/1.jpg"
+	}
+]
+
+
+var imageCellsArray = document.getElementsByClassName('cell__image');
+console.log(imageCellsArray)
+
+for (var i = 0; i < imageCellsArray.length; i++) {
+	var imageCell = imageCellsArray[i];
+
+	console.log(imageCell)
+}
+
+
+
+
+for (var i = 0; i < data.length; i++) {
+	console.log(data[i])
+}
+
+
+
+var cardBox = document.getElementsByClassName('cardBox')[0];
+var card = cardBox.getElementsByClassName('card')[0];
+
+card.addEventListener('click', function (e) {
+	e.preventDefault();
+	this.classList.toggle('is-flipped');
+})
